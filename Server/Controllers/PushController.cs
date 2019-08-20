4bd0a2f0b8c34f8bb0745070584d81ede5ada977
ishowfun <ishowfun@qq.com>
2019-08-20 23:28:56 +0800

    update

---
+++ Server/Controllers/PushController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("[controller]")]
+    public class PushController : Controller
+    {
+        [HttpPost]
+        public ActionResult<Result> Post([FromBody]MessageModel messageModel)
+        {
+
+            return new Result();
+        }
+    }
+}
---
+++ Server/Models/MessageModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server.Models
+{
+    public class MessageModel
+    {
+        public string za { get; set; }
+        public string zb { get; set; }
+        public string zc { get; set; }
+        public string zd { get; set; }
+        public string ze { get; set; }
+        public string zf { get; set; }
+        public string zg { get; set; }
+        public string zh { get; set; }
+        public string zi { get; set; }
+        public string zj { get; set; }
+        public string zk { get; set; }
+        public string zl { get; set; }
+        public string a { get; set; }
+        public string b { get; set; }
+        public string c { get; set; }
+        public string d { get; set; }
+        public string e { get; set; }
+        public string f { get; set; }
+        public string g { get; set; }
+        public string h { get; set; }
+        public string i { get; set; }
+        public string j { get; set; }
+        public string k { get; set; }
+        public string m { get; set; }
+        public string n { get; set; }
+        public string o { get; set; }
+        public string p { get; set; }
+        public string q { get; set; }
+        public string t { get; set; }
+        public string u { get; set; }
+        public string v { get; set; }
+        public string w { get; set; }
+        public string x { get; set; }
+        public string y { get; set; }
+        public string z0 { get; set; }
+        public string z1 { get; set; }
+        public string z2 { get; set; }
+        public string _a { get; set; }
+        public string _b { get; set; }
+        public string _c { get; set; }
+        public string _d { get; set; }
+        public string _e { get; set; }
+        public string _f { get; set; }
+        public string _g { get; set; }
+        public string _h { get; set; }
+        public string _i { get; set; }
+        public string _j { get; set; }
+        public string _k { get; set; }
+
+    }
+}

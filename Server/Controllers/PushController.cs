5426c7acc85a231bce9a1d35ae622b149b986d34
wangxf <wxf@wingoht.com>
2019-08-23 15:09:30 +0800

    add log

--- Server/Controllers/PushController.cs
+++ Server/Controllers/PushController.cs
@@ -19,7 +19,7 @@ namespace Server.Controllers
     {
         private DBHelper _db;
         private ILogger _logger;
-        public PushController(DBHelper dBHelper,ILogger<PushController> logger)
+        public PushController(DBHelper dBHelper, ILogger<PushController> logger)
         {
             _db = dBHelper;
             _logger = logger;
@@ -32,13 +32,15 @@ namespace Server.Controllers
             Request.Body.Position = 0;
             Request.Body.CopyTo(memery);
             memery.Position = 0;
-
-            //MessageModel messageModel = JsonConvert.DeserializeObject<MessageModel>(message);
+            string sql = string.Empty;
             string recData = new StreamReader(memery, UTF8Encoding.UTF8).ReadToEnd();
-            DateTime time = ParseTime(double.Parse(messageModel.zd));
-            string sql = $"IF NOT EXISTS (SELECT DeviceId from dbo.GuangFuRec where DeviceId = '{messageModel.g}') INSERT INTO dbo.GuangFuRec (GuangFuRecId,DeviceId,Time,RecData) VALUES({int.Parse(messageModel.za)},'{messageModel.g}','{time.ToString()}','{recData}') else update dbo.GuangFuRec set Time = '{time.ToString()}',RecData='{recData}' where DeviceId = '{messageModel.g}'";
             try
             {
+                //MessageModel messageModel = JsonConvert.DeserializeObject<MessageModel>(message);
+                
+                DateTime time = ParseTime(double.Parse(messageModel.zd));
+                sql = $"IF NOT EXISTS (SELECT DeviceId from dbo.GuangFuRec where DeviceId = '{messageModel.g}') INSERT INTO dbo.GuangFuRec (GuangFuRecId,DeviceId,Time,RecData) VALUES({int.Parse(messageModel.za)},'{messageModel.g}','{time.ToString()}','{recData}') else update dbo.GuangFuRec set Time = '{time.ToString()}',RecData='{recData}' where DeviceId = '{messageModel.g}'";
+
                 if (_db.ExecuteNonQuery(CommandType.Text, sql) == 1)
                 {
                     return new ActionResult<Result>(new Result() { Status = "ACCEPTED" });
@@ -48,12 +50,13 @@ namespace Server.Controllers
                     return new ActionResult<Result>(new Result() { Status = "NODATA" });
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _logger.LogError($"{ex.Message}\n{ex.StackTrace}");
-                _logger.LogDebug(sql);
+                _logger.LogInformation(recData);
+                _logger.LogInformation(sql);
                 return new StatusCodeResult(503);
-            }         
+            }
         }
         private DateTime ParseTime(double utc)
         {
@@ -67,5 +70,5 @@ namespace Server.Controllers
         }
     }
 
-    
+
 }
